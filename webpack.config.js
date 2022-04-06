05e000e101f77139b30b5205e53492c6bb30fb35
juyi212 <dea8307@naver.com>
2022-04-06 15:29:52 +0900

    배포 설정

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
       test: /\.(png|jpe?g|gif)$/i,
       loader: 'file-loader',
       options: {
-        name: 'dirname/[contenthash].[ext]',
+        name: 'assets/[contenthash].[ext]'
       }
     },
     {
