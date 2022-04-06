d0d8cdf69b3b096f1ff74aa961b63f86cfd84456
juyi212 <dea8307@naver.com>
2022-04-06 15:16:33 +0900

    배포 설정

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, './dist'),
     filename: '[name].js',
-    publicPath: '/dist',
+    publicPath: 'dist/',
   },
   devServer: {
     devMiddleware: { publicPath: '/dist' },
